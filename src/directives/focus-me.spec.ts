662631a55de6e07b5bc9b365c03b5eb89451ecbf
Andrea Stagi <stagi.andrea@gmail.com>
2017-09-02 18:05:17 +0200

    Add tests for focusMe directive

--- src/directives/focus-me.spec.ts
+++ src/directives/focus-me.spec.ts
@@ -1,18 +1,44 @@
 
-import {} from 'jasmine';
-
 import {FocusMe} from './focus-me';
 
-import {Component} from '@angular/core';;
-import { TestBed } from '@angular/core/testing';
-
-describe('1st tests', () => {
-  console.log('ciao')
-  it('true is true', () => expect(true).toBe(true));
-});
+import {Component} from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
 
 
-describe('2st tests', () => {
-  console.log('ciao')
-  it('true is true', () => expect(true).toBe(true));
+@Component({
+  selector: 'searchbox',
+  template: `Search: <input type="text" focusMe/>`
+})
+export class SearchTestBox {}
+
+describe('FocusMe attribute directive Tests', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SearchTestBox,
+        FocusMe
+      ]
+    });
+  }));
+
+  afterEach(async(() => {
+    
+  }));
+
+  it('should be able to test directive', async(() => {
+
+    TestBed.compileComponents().then(() => {
+      const fixture = TestBed.createComponent(SearchTestBox);
+      fixture.whenStable().then(() => {
+        let compiled = fixture.debugElement.nativeElement;
+        let input = compiled.childNodes[1];
+        expect(input === document.activeElement).toBe(true);
+      });
+
+    }); 
+
+  }));
+
 });
